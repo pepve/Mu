25f501a14d9037a6c95e6d2e9b22752878b127f0
Pepijn Verlaan <pepijnverlaan@yahoo.com>
2012-04-16 18:52:16 +0300

    provide an async interface to lambdas in the view

--- test/examples/section_function.js
+++ test/examples/section_function.js
@@ -1,5 +1,15 @@
 {
   foo: function (text) {
     return text.toUpperCase();
-  }
+  },
+  bar: function (text, cb) {
+    cb(text.replace('r', 'z'));
+  },
+  complex: function (text, cb) {
+    var rendered = '';
+    mu.renderText(text, this)
+      .on('data', function (data) { rendered += data; })
+      .on('end', function () { cb(rendered.replace('.', '!')); });
+  },
+  what: 'complex'
 }
